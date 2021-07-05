4e5d9cfeb67f23fa1bc737e4e2e072690f470632
jihye kang <jihye59kang@gmail.com>
2021-07-06 00:14:08 +0100

    set api

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,8 +6,18 @@ const Search = () => {
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('search');
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
         }
+
+        search();
         /*(async () => {
             await axios.get('search');
         })();*/
