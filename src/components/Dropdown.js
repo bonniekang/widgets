8d051ac72517e732af561054321c294980041815
jihye kang <jihye59kang@gmail.com>
2021-07-12 00:49:42 +0100

    body event refactoring

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -6,13 +6,19 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
     const ref = useRef();
 
     useEffect(() => {
-        document.body.addEventListener('click', (event) => {
-            if(ref.current.contains(event.target)){
+
+        const onBodyClick = (event) => {
+        if(ref.current.contains(event.target)){
                 return;
             }
             setOpen(false);
-        },
-        { capture: true })
+        }
+
+        document.body.addEventListener('click', onBodyClick, { capture: true })
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick, { capture: true })
+        }
     }, [])
 
     const renderedOptions = options.map((option) => {
