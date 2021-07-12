69473a79fa29a4d902b2edd9125f6fff0a1b2afd
jihye kang <jihye59kang@gmail.com>
2021-07-13 00:08:05 +0100

    detecting url

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,4 +1,20 @@
+import {useEffect} from 'react';
+
 const Route = ({path, children}) => {
+
+    const onLocationChange = () => {
+
+    }
+
+    useEffect(() => {
+        window.addEventListener('popstate', onLocationChange)
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange)
+        }
+
+    }, [])
+
     return window.location.pathname === path ? children : null;
 }
 
