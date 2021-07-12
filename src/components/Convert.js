3df53bc8f12d4fcb3dff79057d59b93edf560f05
jihye kang <jihye59kang@gmail.com>
2021-07-12 22:53:16 +0100

    using translate post api

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const Convert = ({language, text}) => {
 
     useEffect(() => {
-        
+        axios.post('googletranslationapi', {}, {
+            params: {
+                q: text,
+                target: language.value,
+                key: 'api key'
+            }
+        })
     }, [language, text]) 
 
     return (
